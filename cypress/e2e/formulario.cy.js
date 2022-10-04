fed7ad841487245035eda653c2fbeec9674a422b
Gabriel Nunes Silva <gabriel.silva@globalsys.com.br>
2022-10-04 16:56:37 -0300

    Finalizado

---
+++ cypress/e2e/formulario.cy.js
@@ -0,0 +1,43 @@
+import Formulario from '../support/pages/Formulario';
+
+const dados = require('../fixtures/dados.json');
+
+describe('Formulário Ticto', () => {
+    beforeEach(() => {
+        Formulario.acessarFormulario();
+    });
+
+    it('Confirmar carregamento da página', () => {
+        cy.screenshot();
+    });
+
+    it('Preenchendo dados inválidos', () => {
+        Formulario.preencherFormulario('Gabriel', 'nao@existe', 'Teste');
+
+        Formulario.mensagemErroCampoNome();
+        Formulario.mensagemErroCampoPassword();
+    });
+
+    it('Preenchendo dados válidos', () => {
+        Formulario.preencherFormulario(dados.nome, dados.email, dados.password);
+
+        Formulario.validarExistenciaNoFormulario(dados.nome);
+    });
+
+    it('Editar dados inválidos', () => {
+        Formulario.editarUsuario(dados.email, 'ivalido', 'nao@existe');
+    });
+
+    it.only('Editar dados inválidos', () => {
+        let nomeEditado = dados.nome + ' editado';
+        let emailEditado = 'email@editado';
+
+        Formulario.editarUsuario(dados.email, nomeEditado, emailEditado);
+
+        Formulario.validarExistenciaNoFormulario(nomeEditado);
+    });
+
+    it('Escluir usuário', () => {
+        Formulario.excluirUsuario(dados.email);
+    });
+});
--- cypress/support/pages/Formulario/index.js
+++ cypress/support/pages/Formulario/index.js
@@ -1,1 +1,70 @@
-export default class Formulario {}
+class Formulario {
+    acessarFormulario() {
+        cy.visit('https://qa-test.ticto.io/');
+    }
+
+    preencherFormulario(nome, email, password) {
+        cy.get('#name').focus().type(nome);
+        cy.get('#email').focus().type(email);
+        cy.get('#password').focus().type(password);
+
+        cy.contains('Cadastrar').click();
+    }
+
+    editarUsuario(emailJaExistente, nome, email) {
+        cy.contains(emailJaExistente)
+            .parent()
+            .children('th')
+            .invoke('text')
+            .then(id => {
+                console.log(id);
+                cy.contains(emailJaExistente)
+                    .parent()
+                    .contains('Ações')
+                    .click();
+                cy.contains(emailJaExistente)
+                    .parent()
+                    .contains('Editar')
+                    .click();
+                cy.get('#e_name' + id)
+                    .clear()
+                    .type(nome);
+                cy.get('#e_email' + id)
+                    .clear()
+                    .type(email);
+
+                cy.get(
+                    '#modalEdit' +
+                        id +
+                        ' > .modal-dialog > .modal-content > .modal-footer > .btn-primary'
+                ).click({ force: true });
+            });
+    }
+
+    excluirUsuario(emailJaExistente) {
+        cy.contains(emailJaExistente).parent().contains('Ações').click();
+        cy.contains(emailJaExistente).parent().contains('Excluir').click();
+
+        cy.contains('Deseja realmente excluir?')
+            .parent()
+            .parent()
+            .contains('Excluir')
+            .click({ force: true });
+    }
+
+    validarExistenciaNoFormulario(dado) {
+        cy.get('table > tbody > tr').children().should('contain.text', dado);
+    }
+
+    mensagemErroCampoNome() {
+        cy.contains('Insira um Nome e Sobrenome válido.').should('be.visible');
+    }
+
+    mensagemErroCampoPassword() {
+        cy.contains('O campo Password deve ter no minimo 8 caracteres.').should(
+            'be.visible'
+        );
+    }
+}
+
+export default new Formulario();
